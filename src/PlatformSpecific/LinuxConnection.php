4fee7f44372806e0735531c5f199da86974c6a43
Damien Walsh <me@damow.net>
2014-01-29 22:46:49 +0000

    Removed implementation until a working build can be tested on Linux.

---
+++ src/PlatformSpecific/LinuxConnection.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Serially
+ * Talk to serial devices in PHP.
+ *
+ * @author Damien Walsh <me@damow.net>
+ */
+
+namespace Serially\PlatformSpecific;
+
+use Serially\AbstractConnection;
+use Serially\ConnectionInterface;
+
+/**
+ * LinuxConnection
+ *
+ * Implements serial communication on the Darwin/BSD (Mac OS X) platform.
+ *
+ * @package Serially
+ */
+abstract class LinuxConnection extends AbstractConnection implements ConnectionInterface
+{
+    /**
+     * @todo Implement me
+     */
+}
