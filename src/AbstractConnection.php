1cc73c8df9808f315906ccd4959e556a480868eb
Damien Walsh <me@damow.net>
2014-01-29 22:39:41 +0000

    Improved documentation.

--- src/AbstractConnection.php
+++ src/AbstractConnection.php
@@ -8,6 +8,9 @@
 
 namespace Serially;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
 /**
  * AbstractConnection
  *
@@ -15,16 +18,38 @@ namespace Serially;
  */
 abstract class AbstractConnection
 {
+    /**
+     * Parity settings
+     */
+    const PARITY_NONE = 0;
+    const PARITY_ODD = 1;
+    const PARITY_EVEN = 2;
+
+    /**
+     * Flow control settings
+     */
+    const FLOW_CONTROL_NONE = 0;
+    const FLOW_CONTROL_RTS_CTS = 1;
+    const FLOW_CONTROL_XON_XOFF = 2;
+
+
     protected $handle = null;
     protected $device = '';
     protected $newLine = PHP_EOL;
+    protected $logger = null;
 
     public function __construct($device)
     {
+        $this->logger = new NullLogger();
         $this->device = $device;
     }
 
-    protected function exec($command)
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    protected function execute($command)
     {
         $desc = array(
             1 => array("pipe", "w"),
@@ -32,16 +57,23 @@ abstract class AbstractConnection
         );
 
         // Execute the command
+        $this->logger->debug('Execute: ' . $command);
         $process = proc_open($command, $desc, $pipes);
 
         // Get stdout and stderr
         $stdOut = stream_get_contents($pipes[1]);
         $stdErr = stream_get_contents($pipes[2]);
 
+        $this->logger->debug('  - stdout: ' . ($stdOut ?: '(empty)'));
+        $this->logger->debug('  - stderr: ' . ($stdErr ?: '(empty)'));
+
         // Close pipes
         fclose($pipes[1]);
         fclose($pipes[2]);
 
-        return proc_close($process);
+        $result = proc_close($process);
+        $this->logger->debug('  - exited: ' . $result);
+
+        return $result;
     }
 }
